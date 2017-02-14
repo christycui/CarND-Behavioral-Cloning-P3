beb95e82c46235facce06abff1c0d3fcdcb776e5
christycui <christycui14@gmail.com>
2017-02-14 09:36:39 -0500

    model improved; car drove further but didn't make pass the first turn

--- clone.py
+++ clone.py
@@ -28,15 +28,19 @@ def generator(samples, batch_size=32):
             images = []
             angles = []
             for batch_sample in batch_samples:
-                name = batch_sample[0].split('/')[-1]
-                center_image = cv2.imread(path+'IMG/'+name)
+                center_image = cv2.imread(path+batch_sample[0])
+                left_image = cv2.imread(path+batch_sample[1].strip())
+                right_image = cv2.imread(path+batch_sample[2].strip())
                 center_angle = float(batch_sample[3])
-                images.append(center_image)
-                angles.append(center_angle)
+                correction = 0.2
+                steering_left = center_angle + correction
+                steering_right = center_angle - correction
+                images.extend([center_image, left_image, right_image])
+                angles.append([center_angle, steering_left, steering_right])
 
             # trim image to only see section with road
             X_train = np.array(images)
-            y_train = np.array(angles)
+            y_train = np.array(angles).reshape([-1,1])
             # add flipped images and angles to avoid bias
             images_flipped = np.fliplr(X_train)
             angles_flipped = -y_train
@@ -57,20 +61,11 @@ from keras.layers import Lambda
 model = Sequential()
 model.add(Cropping2D(cropping=((50,20), (0,0)), input_shape=(160,320,3)))
 model.add(Lambda(lambda x: (x / 255.0) - 0.5))
-# model.add(Convolution2D(24, 5, 5, activation='relu'))
-#model.add(Convolution2D(36, 5, 5, activation='relu'))
-#model.add(Convolution2D(48, 5, 5, activation='relu'))
-#model.add(Convolution2D(64, 3, 3, activation='relu'))
-#model.add(Convolution2D(64, 3, 3, activation='relu'))
 model.add(Convolution2D(32, 5, 5))
 model.add(MaxPooling2D((5, 5)))
 model.add(Dropout(0.5))
 model.add(Activation('relu'))
 model.add(Flatten())
-#model.add(Dense(1164))
-#model.add(Dense(100))
-#model.add(Dense(50))
-#model.add(Dense(10))
 model.add(Dense(1))
 
 model.compile(optimizer='adam',loss='mse')
