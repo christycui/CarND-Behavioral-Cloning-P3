62957c185e88c7b391743a6f72927f6a8ce24e31
christycui <christycui14@gmail.com>
2017-04-01 03:13:46 -0700

    operand type error

--- thunderhill.py
+++ thunderhill.py
@@ -28,18 +28,19 @@ with open(path+'output_processed.txt','r') as log_1:
 from sklearn.model_selection import train_test_split
 train_samples, validation_samples = train_test_split(samples, test_size=0.2)
 
-def generator(samples, batch_size=32):
+def generator(samples, batch_size=16):
   num_samples = len(samples)
   while 1: # Loop forever so the generator never terminates
     sklearn.utils.shuffle(samples)
     for offset in range(0, num_samples, batch_size):
       batch_samples = samples[offset:offset+batch_size]
 
-      images = []
-      speed_input = []
-      cte_input = []
+      input_all = []
       output = []
-      for batch_sample in batch_samples:
+      imageArr = np.zeros((batch_size*2,480,960,3))
+      speedArr = np.zeros((batch_size*2,1))
+      outputArr = np.zeros((batch_size*2,2))
+      for i,batch_sample in enumerate(batch_samples):
         center_angle = float(batch_sample[-4])
         # check steering distribution
         if center_angle == 0:
@@ -54,34 +55,33 @@ def generator(samples, batch_size=32):
           throttle = -brake
 
         img = cv2.imread(path+batch_sample[0])
-        center_image = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
-        #single_input = input_all.append(np.array([center_image,speed,CTE]))
+        center_image = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)[::-1,:,:]
+        imageArr[i*2] = center_image
+        speedArr[i*2] = speed
+        outputArr[i*2] = np.array([center_angle,throttle])
+        #input_all.append([center_image,speed])
         center_image_T = adjust_brightness(center_image)
-        images.extend([center_image, center_image_T])
-        print(len(images))
-        #images = np.array(images)
+        imageArr[i*2+1] = center_image_T
+        speedArr[i*2+1] = speed
+        outputArr[i*2+1] = np.array([center_angle,throttle])
+        # input_all.append(center_image_T,speed])
 
-        speed_input.extend([speed,speed])
-
-
-        single_output = [center_angle,throttle]
-        #input_all.append(np.array([center_image_T, speed, CTE]))
-        output.extend([single_output, single_output])
+        #single_output = [center_angle,throttle]
+        #output.extend([single_output, single_output])
 
       #X_train = np.array(input_all)
-      X_train = [images,speed_input,cte_input]
-      y_train = np.array(output).reshape([-1,2])
+      #y_train = np.array(output).reshape([-1,2])
       #print(X_train.shape)
       # add flipped images and angles to avoid bias
       # print(input_all)
-      #images_flipped = np.array([np.array([np.fliplr(image),speed,cte]) for [image,speed,cte] in input_all])
+      #flipped = np.array([np.array([np.fliplr(image),speed]) for [image,speed] in input_all])
       #print(images_flipped.shape)
-      angles_flipped = np.array([[-angle,throttle] for [angle,throttle] in output]).reshape([-1,2])
-      #X_train = np.concatenate((X_train, images_flipped), axis=0)
-      y_train = np.concatenate((y_train, angles_flipped), axis=0)
-      #print(X_train)
-      #print(X_train)
-      yield sklearn.utils.shuffle(X_train, y_train)
+      #angles_flipped = np.array([[-angle,throttle] for [angle,throttle] in output]).reshape([-1,2])
+      #X_train = np.concatenate((X_train, flipped), axis=0)
+      #X_train = np.array(X_train).reshape(-1,2)
+      #y_train = np.concatenate((y_train, angles_flipped), axis=0)
+
+      yield ({'inputImg': imageArr, 'input_speed': speedArr}, {'output': outputArr})
 
 def adjust_brightness(image):
     rand = random.uniform(0.4, 1.5)
@@ -97,10 +97,11 @@ from keras.layers import Dense, Activation, Flatten, Cropping2D, Dropout
 from keras.layers.convolutional import Convolution2D
 from keras.layers.pooling import MaxPooling2D
 from keras.layers import Lambda, merge, Input
+from keras import backend
 
 row, col, ch = 480,960,3
 
-input_image = Input(shape=(row,col,ch))
+input_image = Input(shape=(row,col,ch),name='inputImg')
 branch_image = Cropping2D(cropping=((50,180), (0,0)), input_shape=(row, col, ch))(input_image)
 branch_image = Lambda(lambda x: (x / 255.0) - 0.5)(branch_image)
 branch_image = Convolution2D(24, 5, 5, subsample=(2,2), activation='relu')(branch_image)
@@ -112,10 +113,10 @@ branch_image = Flatten()(branch_image)
 
 input_speed = Input(shape=(1,),name='input_speed')
 
-output = merge([branch_image, input_speed], mode='concat', concat_axis=1)
+output = backend.concatenate([branch_image, input_speed])
 output = Dense(2)(output)
 
-model =  Model(input = [input_image, input_speed], output = [output])
+model =  Model(input = [input_image, input_speed], output = output)
 
 model.compile(optimizer='adam',loss='mse')
 model.fit_generator(train_generator, samples_per_epoch=len(train_samples), validation_data=validation_generator, nb_val_samples=len(validation_samples), nb_epoch=3)
